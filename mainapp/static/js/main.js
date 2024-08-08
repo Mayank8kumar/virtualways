6e8f416ffc5d0faec5af2c1a62b06828089ce279
unknown <arya@getwise.in>
2024-08-09 02:13:05 +0530

    first commit

---
+++ mainapp/static/js/main.js
@@ -0,0 +1,189 @@
+/*  ---------------------------------------------------
+    Theme Name: Staging
+    Description: Staging bootstrap tamplate
+    Author: Colorib
+    Author URI: https://www.colorib.com/
+    Version: 1.0
+    Created: Colorib
+---------------------------------------------------------  */
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+    });
+
+    /*------------------
+        Background Set
+    --------------------*/
+    $('.set-bg').each(function () {
+        var bg = $(this).data('setbg');
+        $(this).css('background-image', 'url(' + bg + ')');
+    });
+
+    //Canvas Menu
+    $(".canvas__open").on('click', function () {
+        $(".offcanvas-menu-wrapper").addClass("active");
+        $(".offcanvas-menu-overlay").addClass("active");
+    });
+
+    $(".offcanvas-menu-overlay").on('click', function () {
+        $(".offcanvas-menu-wrapper").removeClass("active");
+        $(".offcanvas-menu-overlay").removeClass("active");
+    });
+
+
+    /*------------------
+		Navigation
+	--------------------*/
+    $(".mobile-menu").slicknav({
+        prependTo: '#mobile-menu-wrap',
+        allowParentLinks: true
+    });
+
+    /*-----------------------
+        Hero Slider
+    ------------------------*/
+    $(".hero__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        dots: true,
+        nav: true,
+        navText: ["<i class='fa fa-angle-left'><i/>", "<i class='fa fa-angle-right'><i/>"],
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        smartSpeed: 1500,
+        autoHeight: false,
+        autoplay: true,
+        mouseDrag: false,
+        onInitialized: function (e) {
+            var a = this.items().length;
+            $("#snh-1").html("<span>01</span><span>" + "0" + a + "</span>");
+            var presentage = Math.round((100 / a));
+            $('.slider__progress span').css("width", presentage + "%");
+        }
+    }).on("changed.owl.carousel", function (e) {
+        var b = --e.item.index,
+            a = e.item.count;
+        $("#snh-1").html("<span> " + "0" + (1 > b ? b + a : b > a ? b - a : b) + "</span><span>" + "0" + a + "</span>");
+
+        var current = e.page.index + 1;
+        var presentage = Math.round((100 / e.page.count) * current);
+        $('.slider__progress span').css("width", presentage + "%");
+    });
+
+    /*--------------------------
+        Project Slider
+    ----------------------------*/
+    $(".project__slider").owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 4,
+        dots: false,
+        nav: true,
+        navText: ["<span class='arrow_carrot-left'><span/>", "<span class='arrow_carrot-right'><span/>"],
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: false,
+        responsive: {
+            320: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            992: {
+                items: 3
+            },
+            1200: {
+                items: 4
+            }
+        }
+    });
+
+    /*-----------------------------
+        Tech stack 
+    -------------------------------*/
+    document.addEventListener("DOMContentLoaded", function() {
+        const itemsContainer = document.querySelector(".tech-stack__items");
+        const scrollButtons = document.querySelectorAll(".tech-stack__scroll-btn");
+    
+        let currentIndex = 0;
+    
+        scrollButtons.forEach(button => {
+            button.addEventListener("click", function() {
+                const direction = this.classList.contains("left") ? -1 : 1;
+                const items = document.querySelectorAll(".tech-stack__item");
+                const itemsToShow = Math.floor(itemsContainer.offsetWidth / items[0].offsetWidth);
+                currentIndex = Math.min(Math.max(currentIndex + direction, 0), items.length - itemsToShow);
+                itemsContainer.style.transform = `translateX(-${currentIndex * (items[0].offsetWidth + 20)}px)`;
+            });
+        });
+    });
+    
+    
+
+
+    /*-----------------------------
+        Testimonial Slider
+    -------------------------------*/
+    $(document).ready(function(){
+        $('.testimonial__carousel').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: true,
+            fade: true,
+            prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left"></i></button>',
+            nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+        });
+    });
+
+    
+    /*---------------------------------
+        Logo Carousel
+    ----------------------------------*/
+    $(".logo__carousel").owlCarousel({
+        loop: true,
+        margin: 80,
+        items: 5,
+        dots: false,
+        nav: false,
+        smartSpeed: 1200,
+        autoHeight: false,
+        autoplay: true,
+        responsive: {
+            320: {
+                items: 2
+            },
+            576: {
+                items: 3
+            },
+            992: {
+                items: 5
+            },
+        }
+    });
+
+    /*------------------
+        Counter
+    --------------------*/
+    $('.count').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
+})(jQuery);
